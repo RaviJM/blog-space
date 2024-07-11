d0947dbfcbf3192f0c32a6287158c38f8730cd33
RaviJM <ravi.mak2003@gmail.com>
2024-07-12 00:59:29 +0530

    add Likes to PostCard

--- frontend-admin/src/components/postCard/PostCard.jsx
+++ frontend-admin/src/components/postCard/PostCard.jsx
@@ -3,6 +3,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const PostCard = ({ post }) => {
+  const likesObject = post.likes;
+  const likesArray = Object.keys(likesObject);
+  const noOfLikes = likesArray.length;
+
   return (
     <Link to={`/posts/${[post._id]}`}>
       <button style={{ border: "solid black" }}>
@@ -10,6 +14,7 @@ const PostCard = ({ post }) => {
         <p>{post.createdAt}</p>
         <p>By {post.author.username}</p>
         <p>{post.author.email}</p>
+        <p>Likes: {noOfLikes}</p>
       </button>
     </Link>
   );
