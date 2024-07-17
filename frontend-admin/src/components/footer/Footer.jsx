2ea5633b4c911c4280cd058614c5d00cb881062b
RaviJM <ravi.mak2003@gmail.com>
2024-07-18 02:18:02 +0530

    add styles to Footer

--- frontend-admin/src/components/footer/Footer.jsx
+++ frontend-admin/src/components/footer/Footer.jsx
@@ -4,18 +4,20 @@ import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="flex justify-center items-center p-4 bg-gray-800 text-white">
       <a
-        href="https://www.linkedin.com/in/your-linkedin-profile"
+        href="https://www.linkedin.com/in/ravimakwana/"
         target="_blank"
         rel="noopener noreferrer"
+        className="mx-4 hover:text-gray-400"
       >
         <FaLinkedin size={30} />
       </a>
       <a
-        href="https://github.com/your-github-profile"
+        href="https://github.com/RaviJM"
         target="_blank"
         rel="noopener noreferrer"
+        className="mx-4 hover:text-gray-400"
       >
         <FaGithub size={30} />
       </a>
