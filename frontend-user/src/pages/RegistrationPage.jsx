300e38bbdf2bad25485c78e0be95f526641d2311
RaviJM <ravi.mak2003@gmail.com>
2024-07-18 22:30:22 +0530

    add styles to RegistrationPage

--- frontend-user/src/pages/RegistrationPage.jsx
+++ frontend-user/src/pages/RegistrationPage.jsx
@@ -18,35 +18,31 @@ const RegistrationPage = () => {
   };
 
   return (
-    <div style={styles.container}>
-      <h1>BLOG SPACE</h1>
-      <button style={styles.button} onClick={handleLoginClick}>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold mb-16">
+        BLOG SPACE - Your Stories, Your Space
+      </h1>
+
+      <button
+        className="bg-blue-500 text-white py-2 px-4 rounded-lg mb-4 hover:bg-blue-600 min-w-24"
+        onClick={handleLoginClick}
+      >
         Login
       </button>
-      <button style={styles.button} onClick={handleSignupClick}>
+      <button
+        className="bg-blue-500 text-white py-2 px-4 rounded-lg mb-4 hover:bg-blue-600 min-w-24"
+        onClick={handleSignupClick}
+      >
         Signup
       </button>
-      <button style={styles.button} onClick={handleGuestClick}>
+      <button
+        className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600"
+        onClick={handleGuestClick}
+      >
         Continue as Guest
       </button>
     </div>
   );
-};
-
-const styles = {
-  container: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    height: "100vh",
-  },
-  button: {
-    margin: "10px",
-    padding: "10px 20px",
-    fontSize: "16px",
-    cursor: "pointer",
-  },
 };
 
 export default RegistrationPage;
