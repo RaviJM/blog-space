b9d72a3be7f6340d68787ae21db230f119ab354d
RaviJM <ravi.mak2003@gmail.com>
2024-07-18 22:18:05 +0530

    add styles to AboutPage

--- frontend-user/src/pages/AboutPage.jsx
+++ frontend-user/src/pages/AboutPage.jsx
@@ -2,24 +2,26 @@ import Navbar from "../components/navbar/Navbar";
 
 function AboutPage() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <div>
-        <p>Hey Guys! I'm Ravi Makwana.</p>
-        <p>
-          I made this website for personal blogging about topics that might
-          interest people around the world.
-        </p>
-        <p>
-          The website is currently limited to just me posting blogs, but in
-          future, the creater version of the same website will allow anyone to
-          upload their own blog posts! Sounds amazing right?
-        </p>
-        <p>
-          Do consider sharing this website with your friends, and help me make
-          this an amazing blog website!
-        </p>
-        <p>Cheers! Have a great day.</p>
+      <div className="flex flex-col flex-grow items-center justify-center bg-gray-100 p-8">
+        <div className="max-w-2xl bg-white p-6 rounded-lg shadow-lg text-center">
+          <p className="text-xl mb-4">Hey Guys! I'm Ravi Makwana</p>
+          <p className="mb-4">
+            I made this website for personal blogging about topics that might
+            interest people around the world.
+          </p>
+          <p className="mb-4">
+            The website is currently limited to just me posting blogs, but in
+            future, the creater version of the same website will allow anyone to
+            upload their own blog posts! Sounds amazing right?
+          </p>
+          <p className="mb-4">
+            Do consider sharing this website with your friends, and help me make
+            this an amazing blog website!
+          </p>
+          <p className="text-lg font-semibold">Cheers! Have a great day.</p>
+        </div>
       </div>
     </div>
   );
