251dea13de54095d459b8f0e14f77e08760030a6
RaviJM <ravi.mak2003@gmail.com>
2024-07-27 12:42:32 +0530

    fix: alert on signup on server error

--- frontend-admin/src/pages/SignupPage.jsx
+++ frontend-admin/src/pages/SignupPage.jsx
@@ -37,7 +37,7 @@ const SignupPage = () => {
       }
     } catch (error) {
       console.error("Signup failed:", error);
-      alert("User already exists! Try another username");
+      alert("Server Error");
     }
   };
 
